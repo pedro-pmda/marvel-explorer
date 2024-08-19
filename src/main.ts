5e0ca3e2afe486635aca54eb9949254bcb611394
Pedro Palazón Martínez de Alegría <pedro.palazon@zooplus.com>
2024-08-19 21:21:39 +0200

    refactor: clean the sample code

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import '@/sytele.css'
+import '@/style.css'
 
 const app = createApp(App)
 
