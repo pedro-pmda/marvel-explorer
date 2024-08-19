ea635183900c78e787886c89a2692a8f688cc276
Pedro Palazón Martínez de Alegría <pedro.palazon@zooplus.com>
2024-08-19 21:14:10 +0200

    buid: add Taiwind to the project

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ['./index.hmtl', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
