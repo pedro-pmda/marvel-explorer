82c7d17ff8dd1bd562a3ee5904bdca5f9fa18f90
Pedro Palazón Martínez de Alegría <pedro.palazon@zooplus.com>
2024-08-24 10:39:03 +0200

    refactor: api composable to manage query and pagination from diferents slugs

--- src/composables/marvelApi.ts
+++ src/composables/marvelApi.ts
@@ -1,17 +1,44 @@
-import type { Comics } from '@/types/marvel'
+import { Path, type Characters, type Comics } from '@/types/marvel.ts'
 
-export const useComics = async (page: number = 0): Promise<Comics> => {
-  const apiKey = import.meta.env.VITE_APP_MARVEL_API_PUBLIC
-  const MARVEL_API = import.meta.env.VITE_APP_MARVEL_API_URL
-  const API_SIGN = `apikey=${apiKey}`
+const MARVEL_API = import.meta.env.VITE_APP_MARVEL_API_URL
+const MARVEL_API_KEY = import.meta.env.VITE_APP_MARVEL_API_PUBLIC
+const API_SIGN = `apikey=${MARVEL_API_KEY}`
+const ITEMS_PER_PAGE = 20
 
-  const ITEMS_PER_PAGE = 20
+const getPagination = (page?: number): string => {
+  return page ? `&offset=${page * ITEMS_PER_PAGE}` : ''
+}
 
-  const pagination = page ? `offset=${page * ITEMS_PER_PAGE}` : ''
-  const requestURI = `http:/${MARVEL_API}/comics?${pagination}&${API_SIGN}`
+const getQuery = (query?: string): string => {
+  return query ? `&${query}` : ''
+}
 
+const getRequestURI = (path: Path, query: string, pagination: string): string => {
+  const apiPath = `http:/${MARVEL_API}/${path}`
+  return `${apiPath}?${API_SIGN}${pagination}${pagination}&`
+}
+
+const usefFetch = async (requestURI: string): Promise<Comics> => {
   const res = await fetch(requestURI)
   const jsonRes = await res.json()
-
   return jsonRes.data
+}
+
+interface ApiOptions {
+  query?: string
+  page?: number
+}
+
+export const useMarvelAPI = async (
+  path: Path,
+  options: ApiOptions
+): Promise<Comics | Characters> => {
+  const pagination = getPagination(options.page)
+  const query = getQuery(options.query)
+  const requestURI = getRequestURI(path, query, pagination)
+  return usefFetch(requestURI)
+}
+
+export const useComics = async (page: number = 0): Promise<Comics> => {
+  return (await useMarvelAPI(Path.COMICS, { page })) as Comics
 }
